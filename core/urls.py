7a54f2ff4ea8675f64b6c260ba1e2bbf12731058
Roshan Kumar <rajputroshansingh579@gmail.com>
2026-09-18 18:50:21 +0530

    Added toast messages

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,6 @@ ACCOUNTS_PREFIX = "accounts"
 
 urlpatterns = [
     # Dashboard
-    path("users", UserListView.as_view(), name="user_list"),
     path("", HomeRenderView.as_view(), name="home"),
      
     # Accounts
